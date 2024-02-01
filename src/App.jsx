316b458670876477882f2b90e84345eb3d30c6b1
vishu bhandari <deevishu1234@gmail.com>
2024-02-01 17:57:45 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import About from './About'
 import Contact from './Contact'
+import Error from './Error'
 
 const App=()=> {
+  const Name=()=>{
+    return(
+      <>
+       <h1> hello , my name is vishu</h1>
+      </>
+    )
+  }
   return (
     <>
-      
+      <Routes>
+        <Route exact path='/' element={<About/>} />
+        <Route exact path='/contact' element={<Contact/>}/>
+        <Route path='/contact/Name' element={<Name/>}/>
+        <Route path='*' element={<Error/>} />
+      </Routes>
     </>
   )
 }
---
+++ src/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error=()=> {
+  return (
+    <>
+        <h1>oops! page not found</h1>
+    </>
+  )
+}
+
+export default Error
