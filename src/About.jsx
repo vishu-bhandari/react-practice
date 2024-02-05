d83b254f75e154029a23d22500110551d9b1ed54
vishu bhandari <deevishu1234@gmail.com>
2024-02-06 00:43:39 +0530

    changed ui of about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-const  About=(props)=> {
+const About = (props) => {
   return (
     <>
-        <h1>hello, i am {props.name} page </h1>
+      <div className="container mx-auto">
+        <h1
+          className="text-center mt-5 
+      "
+        >
+          hello, i am {props.name} page
+        </h1>
+        <br />
+        <div className="text-center">
+          <button className="btn btn-outline-success ">
+            welcome to future
+          </button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
